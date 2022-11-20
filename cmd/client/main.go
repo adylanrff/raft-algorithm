2616f95df8568f56da55089848eac59d260ca4ce
Adylan Roaffa <adylan.dev@gmail.com>
2022-11-20 20:45:49 +0800

    implement rpcClient and refactor

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net"
 
 	raftPb "github.com/adylanrff/raft-algorithm/proto/raft"
 	serverPb "github.com/adylanrff/raft-algorithm/proto/server"
 	"github.com/adylanrff/raft-algorithm/raft"
-	"github.com/adylanrff/raft-algorithm/server"
+	"github.com/adylanrff/raft-algorithm/rpc"
 	"github.com/adylanrff/raft-algorithm/util"
 )
 
@@ -18,12 +17,14 @@ var logPath string
 func init() {
 	flag.IntVar(&port, "port", 8000, "server target port")
 	flag.StringVar(&logPath, "log_path", "server.log", "log path")
+
+	flag.Parse()
 }
 
 func main() {
 	util.InitLogger(logPath)
 
-	raftMsg := server.ServerMessageDTO{
+	raftMsg := rpc.ServerMessageDTO{
 		ServerMessage: &serverPb.ServerMessage{
 			Method: raft.RaftMethodName_RequestVotes,
 			Payload: &serverPb.ServerMessage_ServerRequest{
@@ -36,26 +37,8 @@ func main() {
 		},
 	}
 
-	conn, err := net.Dial("tcp", fmt.Sprintf("0.0.0.0:%d", port))
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
+	rpcClient := rpc.NewRPCClient()
+	resp, err := rpcClient.Call(fmt.Sprintf("0.0.0.0:%d", port), &raftMsg)
 
-	byteRaftMsg, err := raftMsg.ToBytes()
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = conn.Write(byteRaftMsg)
-	if err != nil {
-		panic(err)
-	}
-
-	raftMsgDTO, err := server.ParseServerMessage(conn)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("%+v", raftMsgDTO)
+	fmt.Printf("resp=%+v,err=%v", resp, err)
 }
--- raft/server_handler.go
+++ raft/server_handler.go
@@ -2,9 +2,9 @@ package raft
 
 import (
 	raftModel "github.com/adylanrff/raft-algorithm/raft/model"
+	"github.com/adylanrff/raft-algorithm/rpc"
 
 	serverPb "github.com/adylanrff/raft-algorithm/proto/server"
-	"github.com/adylanrff/raft-algorithm/server"
 )
 
 // TODO: This sucks... do a better way to handle various request/response
@@ -21,14 +21,14 @@ func NewRaftServerhandler(raftHandler Raft) *RaftServerHandler {
 	}
 }
 
-func (h *RaftServerHandler) AppendEntriesHandler(req *server.ServerMessageDTO) (resp *server.ServerMessageDTO, err error) {
+func (h *RaftServerHandler) AppendEntriesHandler(req *rpc.ServerMessageDTO) (resp *rpc.ServerMessageDTO, err error) {
 	appendEntriesDTO := &raftModel.AppendEntriesRequestDTO{
 		AppendEntriesRequest: req.GetServerRequest().GetAppendEntriesRequest(),
 	}
 
 	appendEntriesResp, appendEntriesErr := h.raftHandler.AppendEntries(appendEntriesDTO)
 
-	resp = &server.ServerMessageDTO{
+	resp = &rpc.ServerMessageDTO{
 		ServerMessage: &serverPb.ServerMessage{
 			Method: req.GetMethod(),
 			Payload: &serverPb.ServerMessage_ServerResponse{
@@ -46,13 +46,13 @@ func (h *RaftServerHandler) AppendEntriesHandler(req *server.ServerMessageDTO) (
 	return
 }
 
-func (h *RaftServerHandler) RequestVoteHandler(req *server.ServerMessageDTO) (resp *server.ServerMessageDTO, err error) {
+func (h *RaftServerHandler) RequestVoteHandler(req *rpc.ServerMessageDTO) (resp *rpc.ServerMessageDTO, err error) {
 	requestVoteDTO := &raftModel.RequestVoteRequestDTO{
 		RequestVoteRequest: req.GetServerRequest().GetRequestVoteRequest(),
 	}
 
 	requestVoteResp, requestVoteErr := h.raftHandler.RequestVote(requestVoteDTO)
-	resp = &server.ServerMessageDTO{
+	resp = &rpc.ServerMessageDTO{
 		ServerMessage: &serverPb.ServerMessage{
 			Method: req.GetMethod(),
 			Payload: &serverPb.ServerMessage_ServerResponse{
